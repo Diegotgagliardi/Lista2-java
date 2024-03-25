d60b337fd7817a764f75821d2c3ce0714449214e
Diego Gagliardi <diegotgagliardi@gmail.com>
2024-03-25 18:12:20 -0300

    Exercicio 04

---
+++ Exercicio04.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exercicio04 {
+    public static void executar(){
+        Scanner leitor = new Scanner(System.in);
+
+        int[] num = new int[5];
+
+        for (int i = 0; i < num.length; i++) {
+            System.out.println("Digite o número : ");
+            num[i] = leitor.nextInt();
+        }
+        
+        int n = 10;
+
+        for (int i = 0; i < num.length; i++) {
+            if(num[i] < n)
+                System.out.println("Menores que N: " + num[i]);
+        }
+    
+        leitor.close();
+    }
+    
+}
